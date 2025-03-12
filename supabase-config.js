c5156c403b2b7f61f4322fd6341e154dc79e3a9f
aglaessio <124213722+aglaessio@users.noreply.github.com>
2025-03-12 15:40:01 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,13 +1,1 @@
-import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://seu-supabase-url.supabase.co'; // Substitua pelo seu URL do Supabase
-const supabaseKey = 'sua-chave-publica'; // Substitua pela sua chave pública do Supabase
-
-if (!supabaseUrl || !supabaseKey) {
-    console.error("Erro: As credenciais do Supabase não foram definidas corretamente.");
-}
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-console.log("Supabase inicializado com sucesso:", supabase);
-
-export default supabase;
