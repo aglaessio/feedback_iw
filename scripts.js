463af500c8fcdf0384b3b72c1e7e9d16a45433af
aglaessio <124213722+aglaessio@users.noreply.github.com>
2025-03-12 14:47:42 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 import supabase from './supabase-config.js';
 
 // Inicializa o EmailJS
-emailjs.init('zqK9tEVCcUHGTfGnW'); // Substitua pelo seu User ID do EmailJS
+emailjs.init('SEU_USER_ID_DO_EMAILJS'); // Substitua pelo seu User ID do EmailJS
 
 document.getElementById('feedbackForm').addEventListener('submit', async function (event) {
     event.preventDefault();
@@ -48,17 +48,22 @@ document.getElementById('feedbackForm').addEventListener('submit', async functio
 
         // Gera o link de visualização
         const reportId = insertedData[0].id; // Supondo que o Supabase retorne o ID do registro
-        const viewLink = `https://aglaessio.github.io/feedback_iw/view-report.html?id=${reportId}`; // Substitua pelo seu link do GitHub Pages
+        const viewLink = `https://seu-usuario.github.io/seu-repositorio/view-report.html?id=${reportId}`; // Substitua pelo seu link do GitHub Pages
 
         // Envia o e-mail com o link de visualização
         const emailParams = {
             link: viewLink
         };
 
-        await emailjs.send('service_c9d89ee', 'template_owuaduf', emailParams); // Substitua pelos seus IDs do EmailJS
+        await emailjs.send('SEU_SERVICE_ID_DO_EMAILJS', 'SEU_TEMPLATE_ID_DO_EMAILJS', emailParams); // Substitua pelos seus IDs do EmailJS
         console.log("E-mail enviado com sucesso!");
 
-        alert('Relatório enviado com sucesso! Um link de visualização foi enviado para o e-mail.');
+        // Envia uma cópia para o WhatsApp
+        const whatsappMessage = `Olá,\n\nUm novo formulário de feedback foi enviado. Acesse o relatório aqui: ${viewLink}`;
+        const whatsappLink = `https://wa.me/5541988272932?text=${encodeURIComponent(whatsappMessage)}`;
+        window.open(whatsappLink, '_blank'); // Abre o link em uma nova aba
+
+        alert('Relatório enviado com sucesso! Um link de visualização foi enviado para o e-mail e WhatsApp.');
         document.getElementById('feedbackForm').reset();
     } catch (error) {
         console.error('Erro no processo:', error);
